ffa381e8d6e3f3f66f4026c36f9cda23c4cd8645
한일석 <x2ever@naver.com>
2019-10-10 01:19:26 +0900

    Create Parking.py

---
+++ Parking.py
@@ -0,0 +1,11 @@
+from V2X import V2X
+
+
+class Parking(V2X):
+    def __init__(self, position, width, height, stay_time=10):
+        V2X.__init__(self, position, name="Parking")
+        self.mission_compelte = False
+        self.time_left = stay_time
+    
+    def update():
+        pass
