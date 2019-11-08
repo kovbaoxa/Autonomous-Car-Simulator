ccb6011ae86e2a384b9fa52ce46cf691e447fa34
x2ever <x2ever@naver.com>
2019-11-08 14:03:53 +0900

    Update Trophy.py

--- Trophy.py
+++ Trophy.py
@@ -1,9 +1,11 @@
 import pygame
 
+
 class TrophySprite(pygame.sprite.Sprite):
     def __init__(self, position):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('images/trophy.png')
+        self.image = pygame.transform.scale(self.image, (70, 70))
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = position
 
