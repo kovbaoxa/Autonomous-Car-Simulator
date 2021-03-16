f4076c353dc7d04d3ac75df9bc6d823c75e148fb
Mauro Guerrera <mauroguerrera@eaton.com>
2021-03-16 19:13:57 +0100

    Simplify control
    Signed-off-by: Mauro Guerrera <mauroguerrera@eaton.com>

--- Control.py
+++ Control.py
@@ -5,35 +5,31 @@ class Control:
     __MAX_TURNING_ANGLE   = 10
 
     def __init__(self):
-        self.__speed_increase  = 0
-        self.__speed_decrease  = 0
-        self.__direction_left  = 0
-        self.__direction_right = 0
+        self.__speed_variation     = 0
+        self.__direction_variation = 0
 
     def faster(self, val:int = 1):
-        self.__speed_increase = val if val <= self.__MAX_SPEED_VARIATION else self.__MAX_SPEED_VARIATION
-        self.__speed_decrease = 0
+        if val >= 0:
+            self.__speed_variation = val if val <= self.__MAX_SPEED_VARIATION else self.__MAX_SPEED_VARIATION
 
     def slower(self, val:int = 1):
-        self.__speed_increase = 0
-        self.__speed_decrease = val if val <= self.__MAX_SPEED_VARIATION else self.__MAX_SPEED_VARIATION
+        if val >= 0:
+            self.__speed_variation = -val if val <= self.__MAX_SPEED_VARIATION else -self.__MAX_SPEED_VARIATION
 
     def left(self, val:int = 1):
-        self.__direction_left = val if val <= self.__MAX_TURNING_ANGLE else self.__MAX_TURNING_ANGLE
-        self.__direction_right = 0
+        if val >= 0:
+            self.__direction_variation = val if val <= self.__MAX_TURNING_ANGLE else self.__MAX_TURNING_ANGLE
 
     def right(self, val:int = 1):
-        self.__direction_left  = 0
-        self.__direction_right = val if val <= self.__MAX_TURNING_ANGLE else self.__MAX_TURNING_ANGLE
+        if val >= 0:
+            self.__direction_variation = -val if val <= self.__MAX_TURNING_ANGLE else -self.__MAX_TURNING_ANGLE
 
     def reset(self):
-        self.__speed_increase  = 0
-        self.__speed_decrease  = 0
-        self.__direction_left  = 0
-        self.__direction_right = 0
+        self.__speed_variation     = 0
+        self.__direction_variation = 0
 
     def speed_variation(self):
-        return self.__speed_increase - self.__speed_decrease
+        return self.__speed_variation
 
     def direction_variation(self):
-        return self.__direction_left - self.__direction_right
+        return self.__direction_variation
